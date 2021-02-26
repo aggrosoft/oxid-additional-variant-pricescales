fe7d5b8e8d1e9f7bf38d52ae894db16858407aff
alexander <coding@aggrosoft.de>
2021-02-26 11:33:32 +0100

    chore: release v1.0.2

--- metadata.php
+++ metadata.php
@@ -10,7 +10,7 @@ $aModule = array(
         'en' => 'Add additional price scales for certain variants'
     ],
     'thumbnail'    => '',
-    'version'      => '1.0.1',
+    'version'      => '1.0.2',
     'author'       => 'Aggrosoft GmbH',
     'extend'      => [
         \OxidEsales\Eshop\Application\Model\Article::class => \Aggrosoft\AdditionalVariantPriceScales\Application\Model\Article::class,
